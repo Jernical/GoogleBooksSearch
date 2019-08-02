810a114591b2e154ae88d068a129fb3cbc69a6c4
Suraj Patel <Surajpatel527@gmail.com>
2019-08-02 14:07:15 -0400

    almost done

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,9 +5,7 @@ function Nav() {
   return (
     <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" id="homeLink" href="/">
-        Book React App
-      </a>
+      <a className="navbar-brand" id="homeLink" href="/">Jerne's Library</a>
      <ul className="navbar-nav">
      <li className="nav-item">
        <a className="nav-link navbar-brand" id="savedLink" href="/bookshelf">Saved Books</a>
